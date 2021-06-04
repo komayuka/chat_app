3620d15fac3afb0546f5c0dca3701a11e2ab66a8
komayuka <komayuka115@gmail.com>
2021-06-04 23:21:45 +0000

    [update]

---
+++ app/assets/javascripts/channels/room_channel.js
@@ -0,0 +1,19 @@
+import consumer form "./consumer"
+
+const appRoom = consumer.subscriptions.create("RoomChannnel", {
+  received(data) {
+    const messages = document.getElementById('messages');
+    messages.insertAdjacentHTML('beforeend', data['message']);
+  },
+  spesk: function(message) {
+    return this.perform('speak', {message: message});
+  }
+});
+
+window.addEventListener("keypress", function(e) {
+  if (e.keyCode === 13) {
+    appRoom.speak(e.target.value);
+    e.target.value = '';
+    e.preventDafault();
+  }
+})
